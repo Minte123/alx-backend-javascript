7e90cd26092b40fa718aba10d193cc434f4911b5
Minte123 <netacademy1986minte@yahoo.com>
2022-12-22 12:19:09 +0300

    Updated Classes

---
+++ 0x02-ES6_classes/1-main.js
@@ -0,0 +1,11 @@
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 1-main.js 
+[
+    ClassRoom { _maxStudentsSize: 19 },
+    ClassRoom { _maxStudentsSize: 20 },
+    ClassRoom { _maxStudentsSize: 34 }
+]
