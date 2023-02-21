0f7aa549e554371daec5173b02b9e3383cda8311
Minte123 <netacademy1986minte@yahoo.com>
2023-02-21 12:56:25 +0300

    display_message

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,11 +1,8 @@
 /**
  * print in STDOUT the string argument
- * @author Mintesnot Mekonen
- * @param {string} str - string to be printed
- * return {void}
  */
-function displayMessage(str) {
-  console.log(str);
+function displayMessage(args) {
+  console.log(args);
 }
 
 module.exports = displayMessage;
